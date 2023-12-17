3537812a7a9ab35a3b727e353da763d6e31558c7
talhakaskaya017 <148577585+talhakaskaya017@users.noreply.github.com>
2023-12-17 15:50:10 +0300

    restorant otomasyonu

--- restorantotomasyonu/AnaMenu.Designer.cs
+++ restorantotomasyonu/AnaMenu.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AnaMenu";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnaMenu));
+            SuspendLayout();
+            // 
+            // AnaMenu
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            Name = "AnaMenu";
+            ResumeLayout(false);
         }
 
         #endregion
